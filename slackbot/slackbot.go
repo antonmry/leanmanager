5d73ed9d8303a8c5885da95ab1d74f86010ababe
antonmry <anton@galiglobal.com>
2016-06-30 20:20:15 +0200

    Alpha version of the scheduler

--- slackbot/slackbot.go
+++ slackbot/slackbot.go
@@ -2,7 +2,9 @@
 package slackbot
 
 import (
+	"fmt"
 	"log"
+	"sort"
 	"strconv"
 	"time"
 
@@ -30,7 +32,7 @@ func LaunchSlackbot(slackTokenArg, teamIDArg, apiserverHostArg string, apiserver
 	log.Println("slackbot: bot connected")
 
 	// Scheduled tasks (daily launching)
-	t := time.NewTicker(600 * time.Second)
+	t := time.NewTicker(60 * time.Second)
 	go func() {
 		for {
 			launchScheduledTasks(ws)
@@ -52,7 +54,55 @@ func LaunchSlackbot(slackTokenArg, teamIDArg, apiserverHostArg string, apiserver
 }
 
 func launchScheduledTasks(ws *websocket.Conn) {
-	log.Printf("slackbot: periodic message sent\n")
+
+	println("DEBUG: Periodic launcher started")
+	//TODO: we have to update once the daily is launched, to avoid problems an infinite loop
+	channelsDailyMap.Lock()
+	defer channelsDailyMap.Unlock()
+
+	for k, v := range channelsDailyMap.d {
+		t := time.Now()
+		// Firt, check there are than 23 hours since the last one
+		// TODO: we have to stablish a limit for the Daily and a Timeout
+		if !v.lastDaily.IsZero() && v.lastDaily.Sub(t).Hours() < 23 {
+			continue
+		}
+
+		// Then check if today is a daily meeting day
+		fmt.Printf("DEBUG: Today is: %s\n", t.Weekday())
+		i := sort.SearchStrings(v.days, string(t.Weekday()))
+		// FIXME: we have to compare Weekdays, not strings!!
+		if !(i < len(v.days) && v.days[i] == t.Weekday().String()) {
+			continue
+		}
+
+		// Check if it's time to start the meeting
+		tReference := time.Date(0, 1, 1, t.Hour(), t.Minute(), t.Second(), 0, time.UTC)
+		if v.startTime.Sub(tReference) > 0 {
+			fmt.Printf("DEBUG: time for the next one: %2.2f\n", v.startTime.Sub(tReference).Seconds())
+			continue
+		}
+
+		// TODO: implement team availability check
+		teamAvailability := true
+
+		if !v.limitTime.IsZero() && v.limitTime.Sub(tReference) > 0 && !teamAvailability {
+			fmt.Printf("DEBUG: time for the next one: %2.2f\n when connected", v.limitTime.Sub(tReference).Seconds())
+			continue
+		}
+		fmt.Println("DEBUG: start meeting")
+
+		m := &Message{
+			ID:      0,
+			Type:    "message",
+			Channel: k,
+			Text:    "",
+		}
+		// FIXME: what happen if we launch two daily here at the same time? m is going to be share!
+		go func() {
+			manageStartDaily(ws, m)
+		}()
+	}
 }
 
 func manageMessage(m Message, botID string, ws *websocket.Conn) {
